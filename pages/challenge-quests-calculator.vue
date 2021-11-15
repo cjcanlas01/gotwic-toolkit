7bdacab9935fa97f25d2fd7055d76aef79cdb6fb
cjcanlas01 <cjcanlas78@gmail.com>
2021-11-15 23:48:13 +0800

    feat: complete initial ver. of challenge quest troop calculator

---
+++ pages/challenge-quests-calculator.vue
@@ -0,0 +1,196 @@
+<template>
+  <v-container>
+    <v-alert type="error" transition="fade-transition" :value="error.status">
+      {{ error.message }}
+    </v-alert>
+    <span class="d-block mb-5 font-weight-bold">TROOPS QUANTITY</span>
+    <v-row>
+      <v-col cols="7">
+        <v-text-field
+          v-model="troopsQuantity"
+          placeholder="0"
+          solo
+          type="number"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <span class="d-block mb-5 font-weight-bold">POINTS REQUIRED</span>
+    <v-radio-group v-model="selectedOption" row @mouseup="clearInputs">
+      <v-radio
+        v-for="(option, index) in options"
+        :key="index"
+        :label="option.label"
+        :value="option.value"
+      ></v-radio>
+    </v-radio-group>
+    <v-row v-if="selectedOption == 'default'">
+      <v-col cols="7">
+        <v-select
+          v-model="defaultPointsRequired"
+          :items="pointsRequiredDefaultOptions"
+          solo
+          clearable
+          @change="computeTroopsCountToTrain"
+        ></v-select>
+      </v-col>
+    </v-row>
+    <v-row v-if="selectedOption == 'custom'">
+      <v-col>
+        <v-text-field
+          v-model="customPointsRequired"
+          placeholder="0"
+          solo
+        ></v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn depressed large @click="computeTroopsCountToTrain">
+          Compute
+        </v-btn>
+      </v-col>
+    </v-row>
+    <span class="d-block mb-5 font-weight-bold">TROOPS COUNT TO TRAIN</span>
+    <v-row>
+      <v-col v-for="(item, index) in troopsKeys" :key="index" cols="4"
+        >{{ troopsResultLabel(item) }} - Total
+        <b>{{ troopsResultTotal(item) }}</b>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col
+        v-for="(troopKey, troopIndex) in troopsKeys"
+        :key="troopIndex"
+        cols="4"
+      >
+        <p
+          v-for="(item, index) in troopsResultBreakdown(troopKey)"
+          :key="index"
+        >
+          {{ item }}
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      error: {
+        status: false,
+        message: '',
+      },
+      troopsQuantity: '',
+      defaultPointsRequired: '',
+      customPointsRequired: '',
+      selectedOption: 'default',
+      options: [
+        {
+          label: 'Default',
+          value: 'default',
+        },
+        {
+          label: 'Custom',
+          value: 'custom',
+        },
+      ],
+      pointsRequiredDefaultOptions: [187200, 163800, 120000, 72000, 40000],
+      troopTierEquivalentPoints: {
+        T3: 5,
+        T4: 15,
+      },
+      troopCountsToTrainResult: {
+        T3: {
+          label: 'Tier 3',
+          breakdown: [],
+          total: 0,
+        },
+        T4: {
+          label: 'Tier 4',
+          breakdown: [],
+          total: 0,
+        },
+      },
+    }
+  },
+  computed: {
+    troopsKeys() {
+      return Object.keys(this.troopCountsToTrainResult)
+    },
+  },
+  methods: {
+    clearInputs() {
+      this.defaultPointsRequired = ''
+      this.customPointsRequired = ''
+      this.troopCountsToTrainResult = {
+        T3: {
+          label: 'Tier 3',
+          breakdown: [],
+          total: 0,
+        },
+        T4: {
+          label: 'Tier 4',
+          breakdown: [],
+          total: 0,
+        },
+      }
+    },
+    troopsResultLabel(index) {
+      return this.troopCountsToTrainResult[index].label
+    },
+    troopsResultTotal(index) {
+      return this.troopCountsToTrainResult[index].total
+    },
+    troopsResultBreakdown(index) {
+      return this.troopCountsToTrainResult[index].breakdown
+    },
+    computeTroopsCountToTrain(event) {
+      if (!this.troopsQuantity) {
+        this.$nextTick(() => {
+          this.clearInputs()
+        })
+        this.error.status = true
+        this.error.message = 'Insert troop quantity first!'
+        window.setTimeout(() => {
+          this.error.status = false
+        }, 1250)
+        return
+      }
+
+      for (const troopTier of Object.keys(this.troopTierEquivalentPoints)) {
+        const sourceOfPointsRequired =
+          this.selectedOption === 'default'
+            ? this.defaultPointsRequired
+            : this.customPointsRequired
+        const troopsCountRequired =
+          sourceOfPointsRequired / this.troopTierEquivalentPoints[troopTier]
+        const divisibleCount = Math.trunc(
+          troopsCountRequired / this.troopsQuantity
+        )
+        const troopsToTrainAmount = []
+        const mainTroopsCount =
+          divisibleCount === 0
+            ? Math.round(troopsCountRequired)
+            : `${Number.parseInt(this.troopsQuantity)} x ${divisibleCount}`
+        troopsToTrainAmount.push(mainTroopsCount)
+        if (this.troopsQuantity < troopsCountRequired) {
+          const troopsToTrainRemainder =
+            troopsCountRequired % this.troopsQuantity
+          troopsToTrainAmount.push(Math.round(troopsToTrainRemainder))
+        }
+        this.troopCountsToTrainResult[troopTier].breakdown = troopsToTrainAmount
+        this.troopCountsToTrainResult[troopTier].total =
+          Math.round(troopsCountRequired).toLocaleString()
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+::v-deep input::-webkit-outer-spin-button,
+::v-deep input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+</style>
